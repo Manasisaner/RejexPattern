d28e7947d8c291293a0fe923c3ec26b62ad993e8
snehasonwane <snehasonwane01@gmail.com>
2022-05-13 22:15:32 +0530

    [Manasi] Add: Rejex Pattern to Validate Pincode

---
+++ RejexDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace RejexDemo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            String pattern = "^[0-9]*[a-zA-Z]{3,}[a-zA-Z0-9]*$";
+            string[] inputs = { "111", "1aaa", "aa11", "bcc", "1a1aab", "1abb23a" };
+            string pincode = "^[0-9]{3}[]*[0-9]{3}$";
+            string[] pinCodeArr = { "422010", "412109", "400820", "1452" };
+            program p = new Program();
+            p.ValidateConseqCharacter(inputs, pattern);
+            Console.WriteLine();
+            p.ValidPincode(pinCodeArr, pincode);
+            Console.Read();
+        }
+
+    }
+
+}
