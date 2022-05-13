e595bb6b74d2e27759ddb7891c1301f05fe0d1f4
snehasonwane <snehasonwane01@gmail.com>
2022-05-13 22:27:29 +0530

    [Manasi] Add: Taking Alphabets at the end

--- RejexDemo/Program.cs
+++ RejexDemo/Program.cs
@@ -8,13 +8,21 @@ namespace RejexDemo
             String pattern = "^[0-9]*[a-zA-Z]{3,}[a-zA-Z0-9]*$";
             string[] inputs = { "111", "1aaa", "aa11", "bcc", "1a1aab", "1abb23a" };
             string pincode = "^[0-9]{3}[]*[0-9]{3}$";
-            string[] pinCodeArr = { "422010", "412109", "400820", "1452" };
+           // string[] pinCodeArr = { "422010", "412109", "400820", "1452" };
+            string[] pinCodeArr = { "422010", "482109", "40C82K", "145c22" };
             program p = new Program();
             p.ValidateConseqCharacter(inputs, pattern);
             Console.WriteLine();
             p.ValidPincode(pinCodeArr, pincode);
             Console.Read();
         }
+        public void ValidateConseqCharacter(string[] arr, string pattern)
+        {
+            Console.WriteLine("Validaing 3 Conseq Characters");
+            Regex regex = new Regex(pattern);
+            IterateLoop(arr, regex);
+        }
+
 
     }
 
